b1a608afafda233114e14d9c144c3c9604c61996
hope <hope@gravylab.co.kr>
2022-05-12 01:45:41 +0900

    Initial commit

---
+++ src/main/java/com/icloud/jpamapping/JpaMappingTestApplication.java
@@ -0,0 +1,13 @@
+package com.icloud.jpamapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMappingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMappingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icloud/jpamapping/JpaMappingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icloud.jpamapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMappingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
